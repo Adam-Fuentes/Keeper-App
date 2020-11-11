d7073bd7260efbe48052363348109f3b4aca0699
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-11-11 07:22:09 +0100

    add notes.js and map this notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return (<div>
-        <Header/>
-        <Note/>
-        <Footer/>
+        <Header />
+        {notes.map(note =>(<Note 
+            key={note.id}
+            title={note.title}
+            content={note.content}
+        />))}
+        <Footer />
     </div>)
 };
 
